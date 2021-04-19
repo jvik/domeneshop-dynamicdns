d827d7b32406d9b33a99c485a01a5d5e3f9dfa7f
Jørgen V <mail@jvik.no>
2021-04-19 12:40:59 +0100

    fix: fixed bug where timestamp wouldn't update.

--- src/logger.js
+++ src/logger.js
@@ -1,25 +1,24 @@
-import { createLogger, format, transports } from 'winston';
-import dayjs from 'dayjs';
+import { createLogger, format, transports } from "winston";
 
-const utc = require('dayjs/plugin/utc')
-const timezone = require('dayjs/plugin/timezone')
+import dayjs from "dayjs";
+
+const utc = require("dayjs/plugin/utc");
+const timezone = require("dayjs/plugin/timezone");
 
 dayjs.extend(utc);
-dayjs.extend(timezone)
+dayjs.extend(timezone);
 
-const { combine, timestamp, json } = format;
+const timezoned = () => dayjs().tz(process.env.TIMEZONE).format("YYYY-MM-DD HH:mm:ss");
 
 const logger = createLogger({
-  level: 'info',
-  format: combine(
-    timestamp({
-      format: dayjs().tz(process.env.TIMEZONE).format('YYYY-MM-DD HH:mm:ss') 
+  level: "info",
+  format: format.combine(
+    format.timestamp({
+      format: timezoned,
     }),
-    json(),
+    format.simple()
   ),
-  transports: [
-    new transports.Console()
-  ]
-})
+  transports: [new transports.Console()],
+});
 
 export default logger;
